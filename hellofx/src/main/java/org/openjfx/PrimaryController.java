7c6e81f06852ef99eaa5de5890b952aeb91099cd
Ye Zhaoliang <zhaoturkkey@163.com>
2021-09-12 23:39:34 +0800

    很有趣的一个案例：javafx实现数据动态监控展示

--- hellofx/src/main/java/org/openjfx/PrimaryController.java
+++ hellofx/src/main/java/org/openjfx/PrimaryController.java
@@ -1,9 +1,15 @@
 package org.openjfx;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
@@ -76,18 +82,201 @@ public class PrimaryController  {
     @FXML
     private void switchToMFTableView() throws IOException {
 //        App.setRoot("materialsFX/TableViewsDemo");
+
+        /**
+         * 数据
+         */
+        ObservableList<XYChart.Data<String,Number>> data = FXCollections.observableArrayList();
+        XYChart.Data<String,Number> n1 = new XYChart.Data<String,Number>("中国",90);
+        XYChart.Data<String,Number> n2 = new XYChart.Data<String,Number>("日本",80);
+        XYChart.Data<String,Number> n3 = new XYChart.Data<String,Number>("朝鲜",20);
+
+        data.addAll(n1,n2,n3);
+        /**
+         * 界面属性设置
+         */
+
+        ObservableList<XYChart.Series<String,Number>> list_data = FXCollections.observableArrayList();
+        XYChart.Series<String,Number> xy = new XYChart.Series<String,Number>();
+        xy.setName("亚洲生产总值");
+        xy.setData(data);
+        list_data.add(xy); /*比较特殊写法*/
+
+        CategoryAxis x = new CategoryAxis();
+        x.setLabel("国家");
+        NumberAxis y = new NumberAxis(0,100,10);
+        y.setLabel("生产总值");
+
+
+        /**
+         * 总界面
+         */
+        BarChart<String,Number> barChart = new BarChart<>(x,y,list_data);
+        barChart.setPrefWidth(400);
+        barChart.setPrefWidth(600);
+
+        Button returnBut = new Button("回到主页");
+        returnBut.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                try {
+                    App.setRoot("primary");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+        AnchorPane an =new AnchorPane();
+        HBox h1 = new HBox();
+//        h1.getChildren().addAll(n1);
+        h1.getChildren().addAll(barChart,returnBut);
+        an.getChildren().add(h1);
+        App.setRootAn(an);
     }
 
     @FXML
     private void switchToMFTreeView() throws IOException {
         // 文件名 大小写敏感
 //        App.setRoot("materialsFX/TreeViewsDemo");
+
+        /**
+         * 数据, 不用ObserableList解决问题了 直接设置XYChart.Series和XYChart.Dta
+         */
+        XYChart.Series<String,Number> xy1 = new XYChart.Series<String,Number>();
+        xy1.setName("中国");
+        XYChart.Series<String,Number> xy2 = new XYChart.Series<String,Number>();
+        xy2.setName("日本");
+        XYChart.Series<String,Number> xy3 = new XYChart.Series<String,Number>();
+        xy3.setName("韩国");
+
+        XYChart.Data<String,Number> chinaGDP = new XYChart.Data<String,Number>("GDP",90);
+        XYChart.Data<String,Number> japenGDP = new XYChart.Data<String,Number>("GDP",80);
+        XYChart.Data<String,Number> KoreaGDP = new XYChart.Data<String,Number>("GDP",20);
+
+        XYChart.Data<String,Number> chinaGNP = new XYChart.Data<String,Number>("GNP",190);
+        XYChart.Data<String,Number> japenGNP = new XYChart.Data<String,Number>("GNP",180);
+        XYChart.Data<String,Number> KoreaGNP = new XYChart.Data<String,Number>("GNP",90);
+
+        xy1.getData().add(chinaGDP);
+        xy1.getData().add(chinaGNP);
+
+        xy2.getData().add(japenGDP);
+        xy2.getData().add(japenGNP);
+
+        xy3.getData().add(KoreaGDP);
+        xy3.getData().add(KoreaGNP);
+        /**
+         * 界面属性设置
+         */
+
+        CategoryAxis x = new CategoryAxis();
+        x.setLabel("国家");
+        NumberAxis y = new NumberAxis(0,100,10);
+        y.setLabel("生产总值");
+
+        /**
+         * 总界面
+         */
+        BarChart<String,Number> barChart = new BarChart<>(x,y);
+        barChart.getData().add(xy1);
+        barChart.getData().add(xy2);
+        barChart.getData().add(xy3);
+        barChart.setTitle("第二种方式");
+        barChart.setPrefWidth(400);
+        barChart.setPrefWidth(600);
+
+        Button returnBut = new Button("回到主页");
+        returnBut.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                try {
+                    App.setRoot("primary");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+        AnchorPane an =new AnchorPane();
+        HBox h1 = new HBox();
+//        h1.getChildren().addAll(n1);
+        h1.getChildren().addAll(barChart,returnBut);
+        an.getChildren().add(h1);
+        App.setRootAn(an);
     }
 
     @FXML
     private void switchToMFListView() throws IOException {
         // 文件名 大小写敏感
 //        App.setRoot("materialsFX/ListViewsDemo");
+
+        /**
+         * 数据, 不用ObserableList解决问题了 直接设置XYChart.Series和XYChart.Dta
+         * 一种Series代表一种颜色
+         * 两种则两种颜色
+         *
+         * 凡是Data里头标签名字一样的都放在一起！
+         */
+        XYChart.Series<String,Number> xy1 = new XYChart.Series<String,Number>();
+        xy1.setName("GDP");
+        XYChart.Series<String,Number> xy2 = new XYChart.Series<String,Number>();
+        xy2.setName("GNP");
+
+        XYChart.Data<String,Number> GDP1 = new XYChart.Data<String,Number>("中国",90);
+        XYChart.Data<String,Number> GDP2 = new XYChart.Data<String,Number>("日本",80);
+        XYChart.Data<String,Number> GDP3 = new XYChart.Data<String,Number>("韩国",20);
+
+        XYChart.Data<String,Number> GNP1 = new XYChart.Data<String,Number>("中国",190);
+        XYChart.Data<String,Number> GNP2 = new XYChart.Data<String,Number>("日本",180);
+        XYChart.Data<String,Number> GNP3 = new XYChart.Data<String,Number>("韩国",90);
+
+        xy1.getData().addAll(GDP1,GDP2,GDP3);
+        xy2.getData().addAll(GNP1,GNP2,GNP3);
+
+
+        /**
+         * 界面属性设置
+         */
+
+        CategoryAxis x = new CategoryAxis();
+        x.setLabel("国家");
+        NumberAxis y = new NumberAxis(0,100,10);
+        y.setLabel("生产总值");
+
+        /**
+         * 总界面
+         */
+        BarChart<String,Number> barChart = new BarChart<>(x,y);
+        barChart.getData().add(xy1);
+        barChart.getData().add(xy2);
+        barChart.setTitle("第三种方式");
+        barChart.setPrefWidth(400);
+        barChart.setPrefWidth(600);
+
+        Button returnBut = new Button("回到主页");
+        returnBut.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                try {
+                    App.setRoot("primary");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+        AnchorPane an =new AnchorPane();
+        HBox h1 = new HBox();
+//        h1.getChildren().addAll(n1);
+        h1.getChildren().addAll(barChart,returnBut);
+        an.getChildren().add(h1);
+        App.setRootAn(an);
+    }
+
+    @FXML
+    private void switchToDynamicDrawing() throws IOException {
+        App.setRoot("testDynamicDrawing");
     }
     @FXML
     private void switchToCombobox() throws IOException {
